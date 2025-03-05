1776877557fbc785e9252c1df9839451735e303e
Andrew A Lee <andrew@andrewleedev.com>
2025-03-05 01:13:30 -0800

    Sets up linting

---
+++ cassess/concepts/flexbox/scripts/app.js
@@ -0,0 +1,39 @@
+function handleStyleChange(evt) {
+  const el = evt.target;
+  const controlName = el.dataset.controls;
+  const container = document.getElementById(controlName);
+  const styleName = el.dataset.style ?? controlName.split('-').slice(1).join('-');
+  let out = '';
+
+  if (el.tagName === 'SELECT') {
+    const owns = el.dataset.owns;
+    const val = el.options[el.selectedIndex].text;
+
+    if (owns) {
+      const isDisabled = val !== 'px';
+      document.getElementById(owns).disabled = isDisabled;
+
+      if (!isDisabled) {
+        return;
+      }
+    }
+
+    out = val;
+  } else if (el.tagName === 'INPUT' && el.type === 'number') {
+    const units = el.dataset.styleUnits ?? '';
+
+    out = `${el.value}${units}`;
+  }
+
+  if (out) {
+    container.style[styleName] = out;
+  }
+}
+
+function init() {
+  document.querySelectorAll('.flex-style-control').forEach((el) => {
+    el.addEventListener('change', handleStyleChange);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ cassess/eslint.config.js
@@ -0,0 +1,53 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import html from '@html-eslint/eslint-plugin';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+
+  {
+    files: ['**/*.html'],
+    ...Object.entries(html.configs['flat/recommended']).reduce((config, [key, values]) => {
+      if (key === 'rules') {
+        config[key] = {
+          ...values,
+          // Misc rules
+          '@html-eslint/indent': ['error', 2],
+          '@html-eslint/require-meta-charset': 'error',
+          // Disable conflicts with Prettier
+          '@html-eslint/attrs-newline': 'off',
+          '@html-eslint/no-extra-spacing-attrs': 'off',
+          '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }],
+          // Enable a11y rules
+          '@html-eslint/no-abstract-roles': 'error',
+          '@html-eslint/no-accesskey-attrs': 'error',
+          '@html-eslint/no-aria-hidden-body': 'error',
+          '@html-eslint/no-heading-inside-button': 'error',
+          '@html-eslint/no-invalid-role': 'error',
+          '@html-eslint/no-nested-interactive': 'error',
+          '@html-eslint/no-non-scalable-viewport': 'error',
+          '@html-eslint/no-positive-tabindex': 'error',
+          '@html-eslint/no-skip-heading-levels': 'error',
+          '@html-eslint/require-button-type': 'error',
+          '@html-eslint/require-form-method': 'error',
+          '@html-eslint/require-frame-title': 'error',
+          '@html-eslint/require-input-label': 'error',
+          '@html-eslint/require-meta-viewport': 'error',
+        };
+      } else {
+        config[key] = values;
+      }
+
+      return config;
+    }, {}),
+  },
+];
