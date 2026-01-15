555ebaa324f052acc06b55feb7cf9ab535fa9502
Andrew A Lee <andrew@andrewleedev.com>
2026-01-15 11:03:45 -0800

    Adds insertion sort algorithm

---
+++ algorhythmics/src/insertion-sort.ts
@@ -0,0 +1,46 @@
+/**
+ * Sorts the specified array in-place using the `Insertion Sort` algorithm.
+ * O(n) best case time. O(n^2) worst case.
+ * O(1) space.
+ *
+ * @remarks
+ * Builds the sorted list one element at a time, similarly to sorting the playing cards in your hand.
+ * Most efficient on small datasets or data that is close to sorted, approaching O(n) performance.
+ *
+ * @param arr - The array to sort.
+ * @returns The sorted array.
+ */
+export function insertionSort(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    const key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+/**
+ * An alternate implementation where a series of swaps is used in lieu of a "key" element.
+ *
+ * @param arr - The array to sort.
+ * @returns The sorted array.
+ */
+export function insertionSortVariation(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      j--;
+    }
+  }
+
+  return arr;
+}
---
+++ algorhythmics/tests/insertion-sort.test.ts
@@ -0,0 +1,78 @@
+import { insertionSort, insertionSortVariation } from '../src/insertion-sort.ts';
+import isSorted from './helpers/is-sorted.ts';
+
+describe('insertionSort', () => {
+  test('Returns an empty array when given an empty array', () => {
+    const arr = [];
+    const result = insertionSort(arr);
+
+    expect(result).toStrictEqual([]);
+  });
+
+  test('Returns an array of one element when given an array of one element', () => {
+    const arr = [1];
+    const result = insertionSort(arr);
+
+    expect(result).toStrictEqual([1]);
+  });
+
+  test('Sorts the array of numbers', () => {
+    const arr = [6, 2, 4, 7, 1, 3];
+    const result = insertionSort(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+
+  test('Sorts a small array', () => {
+    const arr = [3, 1];
+    const result = insertionSort(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+
+  test('Sorts a large array', () => {
+    const length = 100;
+    const arr = new Array(length).fill(null).map((_, index) => length - index);
+    const result = insertionSort(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+});
+
+describe('insertionSortVariation', () => {
+  test('Returns an empty array when given an empty array', () => {
+    const arr = [];
+    const result = insertionSortVariation(arr);
+
+    expect(result).toStrictEqual([]);
+  });
+
+  test('Returns an array of one element when given an array of one element', () => {
+    const arr = [1];
+    const result = insertionSortVariation(arr);
+
+    expect(result).toStrictEqual([1]);
+  });
+
+  test('Sorts the array of numbers', () => {
+    const arr = [6, 2, 4, 7, 1, 3];
+    const result = insertionSortVariation(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+
+  test('Sorts a small array', () => {
+    const arr = [3, 1];
+    const result = insertionSortVariation(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+
+  test('Sorts a large array', () => {
+    const length = 100;
+    const arr = new Array(length).fill(null).map((_, index) => length - index);
+    const result = insertionSortVariation(arr);
+
+    expect(isSorted(result)).toBeTruthy();
+  });
+});
