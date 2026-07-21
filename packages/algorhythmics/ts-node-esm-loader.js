972a1a543c83a462f46e8d0d5b7648f28c5fb00b
Andrew A Lee <andrew@andrewleedev.com>
2026-07-21 09:44:17 -0700

    Replaces ts-node with tsx

--- packages/algorhythmics/ts-node-esm-loader.js
+++
@@ -1,7 +0,0 @@
-// Workaround for the ts-node `Unknown file extension ".ts"` error with usage of ESM in Node.
-// Ts-node must be run via NPM script to invoke this custom loader.
-// E.g., `npm run ts-node <file_path>`.
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-register('ts-node/esm', pathToFileURL('./'));
