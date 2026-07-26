b2927339c808450b06dc9220fb43bf9b060b6c2a
Andrew A Lee <andrew@andrewleedev.com>
2026-07-25 23:55:05 -0700

    Adds Letter Distance problem

---
+++ packages/algorhythmics/src/experiments/omit-props.ts
@@ -0,0 +1,39 @@
+/**
+ * Omits the properties from the specified object.
+ *
+ * @param obj - Object to modify.
+ * @param props - List of properties to omit.
+ * @returns Copy of the object with properties omitted.
+ */
+export function omitProps<T extends object, U extends string>(obj: T, props: U[]) {
+  const omittedObj = {} as {
+    [Property in keyof T as Exclude<Property, U>]: T[Property];
+  };
+
+  for (const [prop, value] of Object.entries(obj)) {
+    if (!props.includes(prop as U)) {
+      const tKey = prop as keyof typeof omittedObj;
+      omittedObj[tKey] = value as (typeof omittedObj)[keyof typeof omittedObj];
+    }
+  }
+
+  return omittedObj;
+}
+
+interface User {
+  email: string;
+  password: string;
+  createdDate: Date;
+  hashedPassword: string;
+}
+
+const user: User = {
+  email: 'andrew@dev.to',
+  password: 'Hax0rZ',
+  createdDate: new Date(),
+  hashedPassword: 'ds0938h4sdk7hjf832rh',
+};
+
+const nUser = omitProps(user, ['hashedPassword', 'password']);
+
+console.log(nUser);
